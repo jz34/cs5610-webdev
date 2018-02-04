ccc6bdc578fe41d072e0e92b5eb0476043ace658
Jiawei Zhang <derek.zjw@gmail.com>
2018-02-03 16:11:17 -0800

    ass21

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/users/login/login.component';
+import {routing} from './app.routing';
+import { ProfileComponent } from './views/users/profile/profile.component';
+import { RegisterComponent } from './views/users/register/register.component';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
