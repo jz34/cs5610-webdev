7a67914b59f942accd5ed89aa84620d165b13dec
Jiawei Zhang <derek.zjw@gmail.com>
2018-04-07 04:43:33 -0700

    ass6 test

--- hello-world/assignment/services/user.service.server.js
+++ hello-world/assignment/services/user.service.server.js
@@ -29,8 +29,8 @@ module.exports = function (app, models) {
   // auth with Facebook
   app.get('/auth/facebook/callback',
     passport.authenticate('facebook', {
-      successRedirect: '/profile',
-      failureRedirect: '/login'
+      successRedirect: 'http://www.google.com',
+      failureRedirect: 'http://www.google.com'
   }));
 
   app.get ('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
